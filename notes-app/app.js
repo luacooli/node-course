791ceb6a663900ce5d546784df6f31fcadb69aca
Luana <lcorreia@vntrx.com>
2022-05-10 14:46:33 -0300

    feat: start using npm

---
+++ notes-app/app.js
@@ -0,0 +1,12 @@
+// const add = require("./utils");
+
+// const sum = add(4, -2)
+
+// console.log(sum);
+
+const validator = require("validator");
+const getNotes = require("./notes");
+
+const msg = getNotes();
+
+console.log(msg);
