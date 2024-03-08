35658a42644a0c51c728efe4cf3e4d1187c47e7b
Luana Correia <luacooli@gmail.com>
2024-03-08 15:03:23 -0300

    feat: mapbox exercise and readme

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# node-course
+# Node Course by udemy
+
+To start the project, you need to have installed on your computer:
+
+- [Node.js](https://nodejs.org/en/) (version >=12)
+- Npm (comes with node.js installation)
+- postman-request or axios
+
+## Project structure
+
+The project is divided into several folders from all exercises of the course. Before start any project run:
+
+```
+bash
+npm i
+```
+
+To see how each exercise works and its solution, go to the corresponding folder in this repository by
+
+```
+bash
+cd 'folder-name'
+```
+
+Then run the server using the command below:
+
+```
+bash
+node app.js
+```
--- weather-app/app.js
+++ weather-app/app.js
@@ -4,6 +4,7 @@ const request = require('postman-request')
 const url =
   'https://api.weatherapi.com/v1/current.json?key=183c0396f871453389c182753240603&q=-23.6979,-46.6967'
 
+// using postman-resquest
 request({ url, json: true }, (err, res) => {
   if (err) {
     throw err
@@ -16,10 +17,32 @@ request({ url, json: true }, (err, res) => {
   )
 })
 
+// using axios
 axios.get(url).then(({ data }) => {
   console.log(
     `Today the weather is ${data.current.condition.text.toLowerCase()} with a temperature of ${
       data.current.temp_c
     } degrees out and it feels like ${data.current.feelslike_c} degrees.`
+  )
+})
+
+// Geocoding
+
+// using postman-resquest
+const limit = 1
+const city = 'Los%20Angeles'
+const token =
+  'pk.eyJ1IjoibHVhY29vbGkiLCJhIjoiY2x0aXJudDU0MGlyYjJxcDlhYnpucTg2eCJ9.YAVPM7wcG_fic_8Dwx6Iww'
+
+const mapBoxUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${city}.json?access_token=${token}&limit=${limit}`
+
+request({ mapBoxUrl, json: true }, (err, res) => {
+  console.log(res.body.features)
+})
+
+// using axios
+axios.get(mapBoxUrl).then(({ data }) => {
+  console.log(
+    `Lat: ${data.features[0].center[1]} \n Lon: ${data.features[0].center[0]}`
   )
 })
